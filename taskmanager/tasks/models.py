2e473042bedbc6d329d19276fc62e1e49ada77d2
Ssalijoshua <ssalijoshua2002@gmail.com>
2025-01-21 15:23:40 +0300

    Creating relationships and adding constraints

--- taskmanager/tasks/models.py
+++ taskmanager/tasks/models.py
@@ -1,7 +1,32 @@
 from django.db import models
-
 from django.db import models
 from django.contrib.auth.models import User
+
+
+class Meta:
+    db_table_comment = "Holds information about tasks"
+
+class Sprint(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(User,related_name='created_sprints', on_delete=models.CASCADE)
+    tasks = models.ManyToManyField('Task', related_name='sprints', blank=True)
+
+    class Meta:
+        constraints = [
+        models.CheckConstraint(check=models.Q(end_date__gt=models.F('start_date')), name='end_date_after_start_date'),
+        ]
+
+class Epic(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(User, related_name='created_epics', on_delete=models.CASCADE)
 
 class Task(models.Model):
     STATUS_CHOICES = [
@@ -17,5 +42,4 @@ class Task(models.Model):
     updated_at = models.DateTimeField( auto_now=True ) 
     creator = models.ForeignKey( User, related_name="created_tasks", on_delete=models.CASCADE )
     owner = models.ForeignKey( User, related_name="owned_tasks", on_delete=models.SET_NULL, null=True, db_comment="Foreign Key to the User who currently owns the task.",)
-class Meta:
-    db_table_comment = "Holds information about tasks"
+    epic = models.ForeignKey(Epic, on_delete=models.CASCADE)
